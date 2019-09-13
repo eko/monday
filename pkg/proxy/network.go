fb16d4803bdc51e4946020c28d96168fe2f51029
Vincent Composieux <vincent@composieux.fr>
2019-09-13 13:38:56 +0200

    Fixed networking ip assignation depending on OS

--- pkg/proxy/network.go
+++ pkg/proxy/network.go
@@ -38,6 +38,35 @@ func getNetworkInterface() (string, error) {
 	return "", errors.New("Unable to find loopback network interface")
 }
 
+// getAddIPCommandWithArgs returns the command (ifconfig, ip, ...) that will be used for the current OS
+// and its associated arguments
+func getAddIPCommandWithArgs(ip string) (string, []string) {
+	var command = "ifconfig"
+	var args []string
+
+	switch runtime.GOOS {
+	case "darwin":
+		args = []string{networkInterface, "alias", ip, "up"}
+
+	case "linux":
+		// Check if "ifconfig" is available
+		_, err := exec.LookPath(command)
+		if err == nil {
+			// "ifconfig" case
+			args = []string{networkInterface, ip, "up"}
+		} else {
+			// "ip" case
+			command = "ip"
+			args = []string{"addr", "add", ip + "/32", "dev", networkInterface}
+		}
+
+	default:
+		panic(fmt.Sprintf("Sorry, it seems your OS (%s) is not available yet.", runtime.GOOS))
+	}
+
+	return command, args
+}
+
 func generateIP(a byte, b byte, c byte, d int, port string) (net.IP, error) {
 	ip := net.IPv4(a, b, c, byte(d))
 
@@ -47,20 +76,7 @@ func generateIP(a byte, b byte, c byte, d int, port string) (net.IP, error) {
 		return net.IP{}, err
 	}
 
-	// Add a new IP address on the network interface
-	command := "ifconfig"
-	var args []string
-
-	switch runtime.GOOS {
-	case "darwin":
-		args = []string{networkInterface, "alias", ip.String(), "up"}
-
-	case "linux":
-		args = []string{networkInterface, ip.String(), "up"}
-
-	default:
-		return net.IP{}, fmt.Errorf("Unable to find your OS: %s", runtime.GOOS)
-	}
+	command, args := getAddIPCommandWithArgs(ip.String())
 
 	for i := d; i < 255; i++ {
 		ip = net.IPv4(a, b, c, byte(i))
@@ -77,16 +93,20 @@ func generateIP(a byte, b byte, c byte, d int, port string) (net.IP, error) {
 				if err != nil {
 					return net.IPv4(a, b, c, byte(i)), nil
 				}
-				conn.Close()
+				if conn != nil {
+					conn.Close()
+				}
 			}
 		}
 
+		// No already assigned IP/Port available, add IP address to the network interface
 		if err := exec.Command(command, args...).Run(); err != nil {
 			return net.IP{}, fmt.Errorf("Cannot run ifconfig command to add new IP address (%s) on network interface '%s': %v", ip.String(), networkInterface, err)
 		}
 
+		// Try to assign port to the newly assigned IP
 		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%s", ip.String(), port))
-		if err == nil {
+		if err != nil {
 			return net.IPv4(a, b, c, byte(i)), nil
 		}
 		if conn != nil {
